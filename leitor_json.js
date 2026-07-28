4a01fbba54b5b0083209ac66d19a2949b515abb8
Davi <davileandrocos@gmail.com>
2026-07-28 19:08:06 -0300

    Função para verificar se a palavra existe no arquivo json

--- leitor_json.js
+++ leitor_json.js
@@ -7,3 +7,28 @@ async function carregarPalavras() {
 
     return palavra;
 }
+
+function palavra_existe(palavra) {
+        let palavra_aux = "";
+
+        palavra.forEach(letra => {
+            palavra_aux += letra;
+        });
+
+        // Busca se a palavras existe na lista de palavras JSON
+
+        // Converte o json em texo
+        try {
+            // Busca o arquivo JSON via rede (pode ser um caminho local ou uma URL)
+            const resposta = await fetch('/palavras.json');
+
+            // Converte a resposta diretamente para um array JavaScript
+            const listaDePalavras = await resposta.json();
+
+            // Verifica se a palavra exata existe na lista (Retorna true ou false)
+            return listaDePalavras.includes(palavra_aux);
+        } catch (erro) {
+            console.error("Erro ao buscar o arquivo JSON:", erro);
+            return false;
+        }
+    }
